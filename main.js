45aa82d03e25229ecc595d1f4dc91c00279363e2
kalimakeshwaran <makeshvijay735@gmail.com>
2023-01-09 17:23:45 +0530

    add

---
+++ main.js
@@ -0,0 +1,81 @@
+
+//1
+function myfunction(a,b){
+ c=a+b;
+return(c);
+}
+console.log(myfunction(10,20))
+
+
+//1
+function myfunction(a,b){
+    c=a+b;
+   return(c);
+   }
+   console.log(myfunction(100,20))
+
+   
+//1
+function myfunction(a,b){
+    c=a+b;
+   return(c);
+   }
+   console.log(myfunction(104,20))
+
+   
+//1
+function myfunction(a,b){
+    c=a+b;
+   return(c);
+   }
+   console.log(myfunction(1044,20))
+
+   
+//1
+function myfunction(a,b){
+    c=a*b;
+   return(c);
+   }
+   console.log(myfunction(10,2048))
+
+   
+//1
+function myfunction(a,b){
+    c=a-b;
+   return(c);
+   }
+   console.log(myfunction(10,2020))
+
+   
+//1
+function myfunction(a,b){
+    c=a-b;
+   return(c);
+   }
+   console.log(myfunction(10,2034))
+
+   
+//1
+function myfunction(a,b){
+    c=a*b;
+   return(c);
+   }
+   console.log(myfunction(106,204))
+
+   
+//1
+function myfunction(a,b){
+    c=a-b;
+   return(c);
+   }
+   console.log(myfunction(103,205))
+
+
+
+   
+//1
+function myfunction(a,b){
+    c=a*b;
+   return(c);
+   }
+   console.log(myfunction(10,20))
